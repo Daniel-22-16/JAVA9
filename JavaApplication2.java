feb49e8a38613541946d85c1b6c2e522cf98aa46
Daniel-22-16 <1475937@senati.pe>
2023-03-24 11:17:35 -0500

    java9

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class JavaApplication2 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        int numero1 = sc.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int numero2 = sc.nextInt();
     }
     }
